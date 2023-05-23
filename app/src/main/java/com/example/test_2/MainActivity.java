2c83e0fbccefb1993dcade943e0275137bfaab0b
Yasin <vyasin96@gmail.com>
2023-05-23 16:50:17 +0330

    first init

---
+++ app/src/main/java/com/example/test_2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.test_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.editTextTextEmailAddress);
+        editText.setText("Y4CN");
+        Date date = new Date();
+        TextView textView = findViewById(R.id.text1);
+        textView.setText(date.toString());
+
+
+
+        //Button Click
+        TextView textView_hello = findViewById(R.id.textView);
+        Button btn1 = findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(v -> {
+            Random random = new Random();
+            textView_hello.setText("Button Clicked !!");
+            textView_hello.setTextColor(Color.rgb(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+            textView.setVisibility(v.INVISIBLE);
+        });
+
+
+        //this will change the Hello World Color Randomly
+
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Random random = new Random();
+//                textView_hello.setTextColor(Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+//            }
+//        });
+
+
+//        btn.setOnClickListener(v -> btnClicked(v));
+//        btn1.setOnClickListener(v ->Toast.makeText(getApplicationContext(),"Clicked", Toast.LENGTH_LONG).show());
+    }
+
+//    private void btnClicked(View v) {
+//        if (v.getId() == R.id.btn1) {
+////            Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_LONG);
+//            Random random = new Random();
+//            textView.setTextColor(Color.rgb(random.nextInt(256),random.nextInt(256),random.nextInt(256)));
+//        }
+//    }
+}
+
